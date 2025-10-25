b3bf66f630dd3fa43251af65c310a897db20c8ad
Kerrhylon Doraath <crispy.lloyd64@gmail.com>
2025-10-25 14:02:01 +0100

    Updated file handler

--- FileHandler.py
+++ FileHandler.py
@@ -1,5 +1,5 @@
 ##File handler
-
+from LOGGER import *
 
 FILE_READ = 0
 FILE_WRITE = 1
@@ -13,7 +13,6 @@ class FileHandle():
         self.name = ""
         self.file = None
 
-
     def openFile(self, mFileName, mode):
         if mode == 0:
             self.file = open(mFileName, "r")
@@ -23,9 +22,12 @@ class FileHandle():
             self.status = "WRITE"
 
     def closeFile(self):
-        if self.status != "CLOSED"
+        if self.status != "CLOSED":
             self.file.close()
             self.status = "CLOSED"
+
+    def writeData(self, info):
+        self.file.write(info)
     
 class FileHandler():
 
@@ -35,21 +37,60 @@ class FileHandler():
         self.handleFormat = "File_"
         self.fileCount = 0
 
+    def getHandleByName(self, name):
+        for handle in handles:
+            if handle.name == name:
+                return handle
+        return None
+
+    def createFile(self, mFileName):
+        mHandle = None
+        for handle in self.handles:
+            if handle.status != "READ" and handle.status != "WRITE":
+                mHandle = handle
+        if mHandle == None:
+            mHandle = FileHandle(self.handleFormat+str(self.fileCount))
+            self.fileCount += 1
+        try:
+            file = open(mFileName, "r")
+            file.close()
+            self.logger.log(f"Attempted to create file {mFileName}. File already exists." , "ERROR")
+            return
+        except OSError:
+            mHandle.file = open(mFileName, "w")
+            mHandle.status = "WRITE"
+            self.logger.log(f"Created file '{mFileName}' successfully!", "SUCCESS")
+            return mHandle
+        
     def openFile(self, mFileName, mode):
         try:
             mHandle = None
-            for handle in handles:
+            for handle in self.handles:
                 if handle.status != "READ" and handle.status != "WRITE":
                     mHandle = handle
             if mHandle == None:
                 mHandle = FileHandle(self.handleFormat+str(self.fileCount))
                 self.fileCount += 1
-                mHandle.openFile(mFileName)
-        except OSError:
+                mHandle.openFile(mFileName, mode)
+        except Exception as e:
+            raise e
             self.logger.log(f"Failed to open file: {mFileName}", "ERROR") 
         except Exception as e:
             self.logger.log(f"Unknown error occurred when opening a file {e}", "ERROR")
         return mHandle
 
     def closeFile(self, pFileHandle):
-        pFileHandle.closeFile()
+        try:
+            pFileHandle.closeFile()
+        except OSError:
+            self.logger.log(f"Failed to close file with handle {pFileHandle.name}", "ERROR")
+        except Exception as e:
+            self.logger.log(f"Unknown error occurred when opening a file {e}", "ERROR")
+
+
+    def readData(self, handle):
+        return handle.file.read()
+
+
+    def writeData(self, handle, info):
+        handle.writeData(info)
